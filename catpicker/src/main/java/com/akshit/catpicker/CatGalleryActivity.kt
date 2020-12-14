f36f527021b7604ad323dad896bb3579833cd169
akshitc <chhabra.dce@gmail.com>
2020-12-14 16:57:36 +0100

    Initial commit to create cat gallery activity

---
+++ catpicker/src/main/java/com/akshit/catpicker/CatGalleryActivity.kt
@@ -0,0 +1,12 @@
+package com.akshit.catpicker
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CatGalleryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cat_gallery)
+    }
+}
