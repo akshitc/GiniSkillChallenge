3c51295adb78381add32fda9821f60907dbd7f75
akshitc <chhabra.dce@gmail.com>
2020-12-14 16:58:18 +0100

    Use catpicker library in sample app

--- app/src/main/java/com/akshit/giniskillchallenge/MainActivity.kt
+++ app/src/main/java/com/akshit/giniskillchallenge/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.akshit.giniskillchallenge
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.akshit.catpicker.CatGalleryActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setListeners()
+    }
+
+    private fun setListeners() {
+        start_button.setOnClickListener {
+            startActivity(Intent(this, CatGalleryActivity::class.java))
+        }
     }
 }
