792425dc0d11eb9495d4f8d9b3025a86c1939571
akshitc <chhabra.dce@gmail.com>
2020-12-14 17:45:43 +0100

    Create repository and paging source

---
+++ catpicker/src/main/java/com/akshit/catpicker/api/CatPagingSource.kt
@@ -0,0 +1,22 @@
+package com.akshit.catpicker.api
+
+import androidx.paging.PagingSource
+import com.akshit.catpicker.model.CatModel
+
+class CatPagingSource(private val catService: CatService) : PagingSource<Int, CatModel>() {
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, CatModel> {
+        return try {
+            val nextPageNumber = params.key ?: 0
+            val limit = params.loadSize
+            val response = catService.getCats(limit = limit, page = nextPageNumber)
+            LoadResult.Page(
+                data = response,
+                prevKey = null, // Only paging forward.
+                nextKey = nextPageNumber + 1
+            )
+        } catch (e: Exception) {
+            LoadResult.Error(e)
+        }
+    }
+}
---
+++ catpicker/src/main/java/com/akshit/catpicker/api/CatRepository.kt
@@ -0,0 +1,18 @@
+package com.akshit.catpicker.api
+
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import com.akshit.catpicker.model.CatModel
+import kotlinx.coroutines.flow.Flow
+
+class CatRepository(private val catService: CatService) {
+
+    fun getCats(): Flow<PagingData<CatModel>> {
+        return Pager(
+            PagingConfig(pageSize = 20, enablePlaceholders = false)
+        ) {
+            CatPagingSource(catService)
+        }.flow
+    }
+}
